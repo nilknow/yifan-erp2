923be3a38dd057013dc056c8ce399f27c0006516
nilknow <maxwangein@gmail.com>
2024-03-29 23:38:14 +0800

    top navbar

--- frontend/yifan-erp2/app/lib/index/TopNavBar.tsx
+++ frontend/yifan-erp2/app/lib/index/TopNavBar.tsx
@@ -1,9 +1,15 @@
 export default function TopNavBar() {
   return (
-    <div
-      className={"flex-row"}>
-      <a className={"text-gray-500 hover:bg-gray-500 hover:text-white px-4 py-4 text-left font-bold transition duration-300"} href="/">返回首页</a>
-      <a className={"text-gray-500 hover:bg-gray-500 hover:text-white px-4 py-4 text-left font-bold transition duration-300"} href="/logout">登出</a>
+    <div className={"flex-row"}>
+      <a href={"/"}>
+        <img src={"/66_long.png"} className={"w-8 h-8 m-5 inline"}/>
+      </a>
+      <a href="/"
+         className={"text-gray-500 hover:bg-neutral-700 hover:text-white px-4 py-4 text-left font-bold transition duration-300"}
+      >返回首页</a>
+      <a href="/logout"
+         className={"text-gray-500 hover:bg-neutral-700 hover:text-white px-4 py-4 text-left font-bold transition duration-300"}
+      >登出</a>
     </div>
   )
 }
